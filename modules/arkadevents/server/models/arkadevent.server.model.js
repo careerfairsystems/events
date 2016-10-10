fb698615f7423ed52a136e9230485cb2c629b35c
Sara Egli <Sara Egli>
2016-10-10 15:39:28 +0200

    Added model for events

--- modules/arkadevents/server/models/arkadevent.server.model.js
+++ modules/arkadevents/server/models/arkadevent.server.model.js
@@ -13,9 +13,42 @@ var ArkadeventSchema = new Schema({
   name: {
     type: String,
     default: '',
-    required: 'Please fill Arkadevent name',
+    required: 'Please fill in a name for the event.',
     trim: true
   },
+  photo: {
+    type: String,
+    default: ''
+  },
+  description: {
+    type: String,
+    required: 'Please fill in a description for the event.'
+  },
+  nrofseats: {
+    type: Number,
+    required: 'Please fill in the maximum number of seats.'
+  },
+  foodserved: {
+    type: Boolean,
+    requried: 'Please fill in if food will be served at the event.'
+  },
+  typeoffood: {
+    type: String
+  },
+  date: {
+    type: Date,
+    required: 'Pick a time and date for the event.'
+  },
+  reminders: [{
+    reminder: Schema.ObjectId,
+    hoursbeforeevent: Number
+  }],
+  enrolled: [
+    Schema.ObjectId
+  ],
+  standby: [
+    Schema.ObjectId
+  ],
   created: {
     type: Date,
     default: Date.now
