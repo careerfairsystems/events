a726255f28c52c7a390f22c840d3aec2a471faee
Sara Egli <Sara Egli>
2016-10-10 15:45:35 +0200

    Added mongo model for reminders Closes #32

--- modules/reminders/server/models/reminder.server.model.js
+++ modules/reminders/server/models/reminder.server.model.js
@@ -16,6 +16,14 @@ var ReminderSchema = new Schema({
     required: 'Please fill Reminder name',
     trim: true
   },
+  emailtitle: {
+    type: String,
+    required: 'Please add an email title.'
+  },
+  emailbody: {
+    type: String,
+    required: 'Please add a message to be sent in the reminder-email'
+  },
   created: {
     type: Date,
     default: Date.now
