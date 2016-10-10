15b1633b158519ea4e4c758c3f5ce56222d373f1
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-11 01:38:30 +0200

    Add db-model for Reservations
    closes #23

--- modules/reservations/server/models/reservation.server.model.js
+++ modules/reservations/server/models/reservation.server.model.js
@@ -16,9 +16,37 @@ var ReservationSchema = new Schema({
     required: 'Please fill Reservation name',
     trim: true
   },
+  guild: {
+    type: String,
+    required: 'Please fill Guild'
+  },
+  year: {
+    type: String,
+    required: 'Please fill year'
+  },
+  email: {
+    type: String,
+    required: 'Please fill the email'
+  },
+  phone: {
+    type: String,
+    required: 'Please fill the email'
+  },
+  foodpref: {
+    type: String,
+    required: 'Please fill the food preferences'
+  },
+  showedup: {
+    type: Boolean,
+    default: false
+  },
   created: {
     type: Date,
     default: Date.now
+  },
+  arkadevent: {
+    type: Schema.ObjectId,
+    ref: 'Arkadevent'
   },
   user: {
     type: Schema.ObjectId,
