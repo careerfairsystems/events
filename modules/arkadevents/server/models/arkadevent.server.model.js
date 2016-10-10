96f0e2bd858dc1b7c1c6fa2990c3204c8960c81b
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-10 16:40:35 +0200

    Add events to frontpage
    Extra Features:
    - redesign to ARKAD
    - add event-Image
    
    closes #12

--- modules/arkadevents/server/models/arkadevent.server.model.js
+++ modules/arkadevents/server/models/arkadevent.server.model.js
@@ -16,6 +16,10 @@ var ArkadeventSchema = new Schema({
     required: 'Please fill Arkadevent name',
     trim: true
   },
+  image: {
+    type: String,
+    default: 'https://s-media-cache-ak0.pinimg.com/originals/5b/2a/0a/5b2a0a845e530ca0f2e38a954b3f4523.jpg'
+  },
   created: {
     type: Date,
     default: Date.now
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,40 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'ArkadeventsService', 'ReservationsService',
+  function ($scope, Authentication, ArkadeventsService, ReservationsService) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    var vm = this;
+    $scope.vm = vm;
+    vm.events = [];
+    vm.reservations = [];
+
+    ArkadeventsService.query(getEventsDone);
+    function getEventsDone (data){
+      vm.events = data;
+      ReservationsService.query(getReservationsDone);
+    }
+    function getReservationsDone (data){
+      vm.reservations = data;
+      getDataDone();
+    }
+
+    function getDataDone(){
+      vm.events.forEach(calcSpotsLeft);
+    }
+
+    function calcSpotsLeft(e){
+      e.spotsLeft = vm.reservations.reduce(reservedSpots, e.totalSpots || 0);
+      function reservedSpots(pre, curr){ return pre - (curr.event === e._id); }
+    }
+
+    function goToEvent(){
+      // TODO: Implement
+    }
+
+    function reservlistToEvent(){
+      // TODO: Implement
+    }
+
   }
 ]);
