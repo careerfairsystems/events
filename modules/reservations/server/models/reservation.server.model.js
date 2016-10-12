4c65371defc74132df4e3b0559e5da751c141510
Sara Egli <Sara Egli>
2016-10-12 18:16:56 +0200

    Added info to reservation model Added enroller, standby, pending and offer. Comments in model describe what they do. Closes #46

--- modules/reservations/server/models/reservation.server.model.js
+++ modules/reservations/server/models/reservation.server.model.js
@@ -53,6 +53,22 @@ var ReservationSchema = new Schema({
   user: {
     type: Schema.ObjectId,
     ref: 'User'
+  },
+  /* true if reservation has been accepted and the person has accepted the spot (is set to false at unregistration)*/
+  enrolled: {
+    type: Boolean
+  },
+  /* true if the reservation was on standby and the person has not been offered a spot */
+  standby: {
+    type: Boolean
+  },
+  /* true if has been offered a spot as stand-by but not yet replied */
+  pending: {
+    type: Boolean
+  },
+  /* date when place was offered (set to time when reservation was made if reservation was not on stand-by) */
+  offer: {
+    type: Date
   }
 });
 
