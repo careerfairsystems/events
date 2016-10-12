3a365e8bb7f00b43d5a2e7840cc258282194c724
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-13 00:12:33 +0200

    rm old code

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -13,31 +13,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     ArkadeventsService.query(getEventsDone);
     function getEventsDone (data){
       vm.events = data;
-      //ReservationsService.query(getReservationsDone);
     }
-    /*
-    function getReservationsDone (data){
-      vm.reservations = data;
-      getDataDone();
-    }
-    */
-
-    function getDataDone(){
-      vm.events.forEach(calcSpotsLeft);
-    }
-
-    function calcSpotsLeft(e){
-      e.spotsLeft = Math.max(vm.reservations.reduce(reservedSpots, e.nrofseats || 0), 0);
-      function reservedSpots(pre, curr){ return pre - (curr.arkadevent === e._id); }
-    }
-
-    function goToEvent(){
-      // TODO: Implement
-    }
-
-    function reservlistToEvent(){
-      // TODO: Implement
-    }
-
   }
 ]);
