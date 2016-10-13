d9a6702291378fd29ab7cbe3d6b7e3dda1d52632
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-13 14:44:58 +0200

    Extract programs to a service

--- modules/reservations/client/controllers/reservations.client.controller.js
+++ modules/reservations/client/controllers/reservations.client.controller.js
@@ -6,9 +6,9 @@
     .module('reservations')
     .controller('ReservationsController', ReservationsController);
 
-  ReservationsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'reservationResolve', 'arkadeventResolve', 'Users'];
+  ReservationsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'reservationResolve', 'arkadeventResolve', 'Users', 'ProgramsService'];
 
-  function ReservationsController ($scope, $state, $window, Authentication, reservation, arkadeventResolve, Users) {
+  function ReservationsController ($scope, $state, $window, Authentication, reservation, arkadeventResolve, Users, ProgramsService) {
     var vm = this;
 
     vm.authentication = Authentication;
@@ -21,49 +21,7 @@
     vm.save = save;
     vm.user = vm.authentication.user;
 
-    var allPrograms = ['Byggteknik med arkitektur / Civil Engineering - Architecture',
-                  'Arkitekt / Architect',
-                  'Arkitekt / Architect',
-                  'Medicin och teknik / Biomedical Engineering',
-                  'Bioteknik / Biotechnology',
-                  'Bioteknik / Biotechnology',
-                  'Kemiteknik / Chemical Engineering',
-                  'Kemiteknik / Chemical Engineering',
-                  'Brandingenjörsutbildning / Fire Protection Engineering',
-                  'Brandingenjörsutbildning / Fire Protection Engineering',
-                  'Byggteknik med arkitektur / Civil Engineering - Architecture',
-                  'Byggteknik med järnvägsteknik / Civil Engineering - Railway Construction',
-                  'Byggteknik med järnvägsteknik / Civil Engineering - Railway Construction',
-                  'Civil Engineering- Road and Traffic Technology / Civil Engineering- Road and Traffic Technology',
-                  'Civil Engineering- Road and Traffic Technology / Civil Engineering- Road and Traffic Technology',
-                  'Väg- och vattenbyggnad / Civil Engineering',
-                  'Väg- och vattenbyggnad / Civil Engineering',
-                  'Datateknik / Computer Science and Engineering',
-                  'Datateknik / Computer Science and Engineering',
-                  'Informations- och kommunikationsteknik / Information and Communication Engineering',
-                  'Informations- och kommunikationsteknik / Information and Communication Engineering',
-                  'Ekosystemteknik / Environmental Engineering',
-                  'Elektroteknik / Electrical Engineering',
-                  'Elektroteknik / Electrical Engineering',
-                  'Teknisk Matematik / Engineering Mathematics',
-                  'Teknisk Nanovetenskap / Engineering Nanoscience',
-                  'Teknisk Fysik / Engineering Physics',
-                  'Teknisk Matematik / Engineering Mathematics',
-                  'Teknisk Fysik / Engineering Physics',
-                  'Teknisk Nanovetenskap / Engineering Nanoscience',
-                  'Ekosystemteknik / Environmental Engineering',
-                  'Industridesign / Industrial Design',
-                  'Industriell ekonomi / Industrial Engineering and Management',
-                  'Industridesign / Industrial Design',
-                  'Industriell ekonomi / Industrial Engineering and Management',
-                  'Lantmäteri / Surveying',
-                  'Maskinteknik med teknisk design / Mechanical Engineering with Industrial Design',
-                  'Maskinteknik med teknisk design / Mechanical Engineering with Industrial Design',
-                  'Maskinteknik / Mechanical Engineering',
-                  'Maskinteknik / Mechanical Engineering',
-                  'Medicin och teknik / Biomedical Engineering',
-                  'Lantmäteri / Surveying'];
-    var set = new Set(allPrograms);
+    var set = new Set(ProgramsService);
     $scope.programs = Array.from(set);
     
     loadUserData();
